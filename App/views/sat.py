d64c30032ee9dafbe967d52d610f5a14c0f265df
Kwasi Edwards <kwasiedwards@gmail.com>
2024-03-23 13:10:08 -0400

    changed naming convention

--- App/views/sat.py
+++ App/views/sat.py
@@ -2,8 +2,8 @@ from flask import Blueprint, redirect, render_template, request, send_from_direc
 from App.models import db
 from App.controllers import create_user
 
-sat = Blueprint('sat', __name__, template_folder='../templates')
+sat_views = Blueprint('sat_views', __name__, template_folder='../templates')
 
-@sat.route('/sat', methods=['GET'])
+@sat_views.route('/sat', methods=['GET'])
 def index_page():
     return render_template('sat.html')
